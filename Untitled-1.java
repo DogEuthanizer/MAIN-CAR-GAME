233f48a958c91e43b99b22f5c6d1808ab0a6485a
DogEuthanizer <121259369+DogEuthanizer@users.noreply.github.com>
2022-12-22 20:01:43 +0000

    Pending changes exported from your codespace

---
+++ Untitled-1.java
@@ -0,0 +1,22 @@
+
+
+boolean start = true;
+
+void setup(){
+    size(700,500);
+    
+    
+}
+    
+void draw(){
+  if (start == true);{
+    fill(0);
+    rect(0,0,700,500);
+        fill(#950005);
+   rect(240,275,200,50);
+
+    fill(#FFFFFF);
+      text("PRESS TO START",260,308);
+  }
+  
+}
